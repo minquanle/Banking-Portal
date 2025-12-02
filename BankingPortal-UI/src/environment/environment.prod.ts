75df9ac5cb1ea4d634e2cb889a7a2d615a715cdd
formerchild05 <electronicncs@gmail.com>
2025-12-02 15:23:35 +0700

    l

--- BankingPortal-UI/src/environment/environment.prod.ts
+++ BankingPortal-UI/src/environment/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: '/api',
   tokenName :  "authToken",
-  origin: "http://ac8b54ecc0414458980e28b09dc6c572-930748356.ap-southeast-1.elb.amazonaws.com" 
+  origin: window.location.origin 
   };
