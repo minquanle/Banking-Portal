48fec917d903a32fdf97d95c3f88d6df7933fdc7
Lê Minh Quân <23020563@vnu.edu.vn>
2025-12-05 11:19:00 +0700

    ..

--- BankingPortal-UI/src/environment/environment.ts
+++ BankingPortal-UI/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: '/api',
+    apiUrl: 'http://localhost:8080/api',
     tokenName :  "authToken",
-    origin: window.location.origin 
+    origin: window.location.origin
   };
